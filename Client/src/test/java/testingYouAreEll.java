8f2591462779ef945115fe355117aad045a43f72
Jonathan <jondiehl22@gmail.com>
2024-08-05 08:52:36 -0400

    Working on these tests

--- Client/src/test/java/testingYouAreEll.java
+++ Client/src/test/java/testingYouAreEll.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.assertTrue;
 
 public class testingYouAreEll {
 
-    private YouAreEll urll;
+    public YouAreEll urll;
 
     @Before
     public void setUp() {
@@ -45,7 +45,7 @@ public class testingYouAreEll {
 
     @Test
     public void testGetMessagesOfUser() {
-        String results = urll.get_messagesOfUser("mnaser");
+        String results = urll.get_messagesOfUser("JonnnD");
         int messageCount = results.split(" ").length;
         // Assuming that the user should have at least one message
         assertTrue("Expected at least one message for user", messageCount > 1);
@@ -53,7 +53,7 @@ public class testingYouAreEll {
 
     @Test
     public void testSendMessage() throws IOException {
-        String results = urll.sendMessage("mnaser", "howdy");
+        String results = urll.sendMessage("JonnnD", "");
         int messageCount = results.split(",").length;
         // Assuming that sending a message returns a list with 5 items
         assertEquals("Expected 5 messages after sending a message", 5, messageCount);
@@ -61,7 +61,7 @@ public class testingYouAreEll {
 
     @Test
     public void testSendMessageToUser() throws IOException {
-        String results = urll.sendMessageToUser("hello world", "mnaser", "mnaser1111");
+        String results = urll.sendMessageToUser("hello world", "JonnnD", "JWDiehl");
         int messageCount = results.split(",").length;
         // Assuming that sending a message to a user returns a list with 5 items
         assertEquals("Expected 5 messages after sending a message to user", 5, messageCount);
